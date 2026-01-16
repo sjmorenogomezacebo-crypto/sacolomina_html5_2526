8b85758c6682bed4a4b37bd6085beaf3f0a43938
jmsanchezgomezibz <jmsanchezgomez@alumnat.iessacolomina.es>
2026-01-16 19:23:52 +0100

    mensaje

--- pages/js/main.js
+++ pages/js/main.js
@@ -14,7 +14,23 @@ const productos = [
     precio: 3900.90,
     moneda: "EUR",
     imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-  }
+  },
+  {
+    id: 3,
+    nombre: "harley davidson",
+    descripcion: "Te despeinara solo si tienes pelo.",
+    precio: 3900.90,
+    moneda: "EUR",
+    imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    id: 4,
+    nombre: "harley davidson",
+    descripcion: "Te despeinara solo si tienes pelo.",
+    precio: 3900.90,
+    moneda: "EUR",
+    imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  },
 ];
 
 const contenedor = document.getElementById("products-grid");
