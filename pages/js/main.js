0bf4539e7eadca8890a6cfbe5431caf647925080
sjmorenogomezacebo-crypto <sjmorenogomezacebo@alumnat.iessacolomina.es>
2026-01-20 20:05:16 +0100

    mensaje

--- pages/js/main.js
+++ pages/js/main.js
@@ -11,25 +11,41 @@ const productos = [
     id: 2,
     nombre: "harley davidson",
     descripcion: "Te despeinara solo si tienes pelo.",
-    precio: 3900.90,
+    precio: 4900.90,
     moneda: "EUR",
-    imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imagen: "https://imagenes.autobild.es/files/image_320_240/uploads/imagenes/2025/11/05/690b455579693.jpeg"
   },
   {
     id: 3,
-    nombre: "harley davidson",
+    nombre: "Jesus motor bike",
+    descripcion: "Te despeinara solo si tienes pelo.",
+    precio: 3900.00,
+    moneda: "EUR",
+    imagen: "https://electyum.com/wp-content/uploads/2024/09/moto-electrica-horwin-ht5.webp"
+  },
+  {
+    id: 4,
+    nombre: "Fatimas motor bike",
     descripcion: "Te despeinara solo si tienes pelo.",
     precio: 3900.90,
     moneda: "EUR",
     imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
-    id: 4,
-    nombre: "harley davidson",
-    descripcion: "Te despeinara solo si tienes pelo.",
-    precio: 3900.90,
+    id: 5,
+    nombre: "Torrot power",
+    descripcion: "Veloz como el viento.",
+    precio: 3000.90,
     moneda: "EUR",
-    imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    imagen: "https://www.mundomotero.com/wp-content/uploads/2025/11/bsa-bantam-350-port.jpg"
+  },
+  {
+    id: 6,
+    nombre: "fast motor bike",
+    descripcion: "8 cilindros en linea.",
+    precio: 5000.90,
+    moneda: "EUR",
+    imagen: "https://motissimo.es/wp-content/uploads/2025/03/e79eda7a7c034dd3879ddb81fdcd9bff-1-600x360.jpg"
   },
 ];
 
