d89a466a87e0da6fba690d06b20a21eb03e7732d
fatimakaddour <fbounif@alumnat.iessacolomina.es>
2026-01-23 18:46:17 +0100

    mensaje

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,19 +1,19 @@
 const productos = [
   {
     id: 1,
-    nombre: "santis motor bike",
-    descripcion: "la moto más rápida del mundo.",
+    nombre: "Santis motor bike",
+    descripcion: "La moto más rápida del mundo.",
     precio: 5900.99,
     moneda: "EUR",
     imagen: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Moby_AV-45.jpg"
   },
   {
     id: 2,
-    nombre: "harley davidson",
-    descripcion: "Te despeinara solo si tienes pelo.",
-    precio: 4900.90,
+    nombre: "Kawasaki Ninja h2r",
+    descripcion: "Superbike de alto rendimiento.",
+    precio: 55000.0,
     moneda: "EUR",
-    imagen: "https://imagenes.autobild.es/files/image_320_240/uploads/imagenes/2025/11/05/690b455579693.jpeg"
+    imagen: "https://www.mundomotero.com/wp-content/uploads/2022/10/kawasaki-h2r-2023.jpg"
   },
   {
     id: 3,
@@ -26,7 +26,7 @@ const productos = [
   {
     id: 4,
     nombre: "Fatimas motor bike",
-    descripcion: "Te despeinara solo si tienes pelo.",
+    descripcion: "Ideal para ciudad y carretera.",
     precio: 3900.90,
     moneda: "EUR",
     imagen: "https://images.unsplash.com/photo-1558980664-2cd663cf8dde?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -47,6 +47,23 @@ const productos = [
     moneda: "EUR",
     imagen: "https://motissimo.es/wp-content/uploads/2025/03/e79eda7a7c034dd3879ddb81fdcd9bff-1-600x360.jpg"
   },
+   {
+    id: 7,
+    nombre: "Ducati Panigale",
+    descripcion: "La moto deportiva definitiva.",
+    precio: 38750.90,
+    moneda: "EUR",
+    imagen: "https://images.ctfassets.net/x7j9qwvpvr5s/2TvZT71640MzivRgP4qSp2/f615d076cf8f85a937d2a12847aa8e37/MY23-Panigale-V4R-Model-Blocks-630x390.png"
+  },
+  {
+    id: 8,
+    nombre: "Aprilia RS660",
+    descripcion: "Potencia y agilidad en una sola moto.",
+    precio: 11898.90,
+    moneda: "EUR",
+    imagen: "https://www.motofichas.com/images/cache/10-aprilia-rs-660-2025-estudio-azul-01-739-a.jpg"
+  },
+
 ];
 
 const contenedor = document.getElementById("products-grid");
